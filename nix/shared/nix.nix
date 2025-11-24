cc71103983cb07040e0b1e79926ae2b55bc55335
Gregory Hapgood <greghap@gmail.com>
2025-11-24 13:08:31 -0800

    Update NixOS

--- nix/shared/nix.nix
+++ nix/shared/nix.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }:
-let username = import ./username.nix { };
-in {
+let
+  username = import ./username.nix { };
+in
+{
   nix = {
     extraOptions = "experimental-features = nix-command flakes";
     gc = {
       automatic = true;
       options = "--delete-older-than 7d";
     };
-    optimise = { automatic = true; };
-    package = pkgs.nixStable;
-    settings = { allowed-users = [ username ]; };
+    optimise = {
+      automatic = true;
+    };
+    package = pkgs.nixVersions.stable;
+    settings = {
+      allowed-users = [ username ];
+    };
   };
 }
