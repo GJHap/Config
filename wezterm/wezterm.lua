c4a00d80182a09fa8d9df69baa501cada47441f5
Gregory Hapgood <greghap@gmail.com>
2026-03-24 21:16:10 -0700

    Update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,32 +4,26 @@ local catppuccin_flavor_file = os.getenv('HOME') .. '/.local/state/catppuccin-fl
 wezterm.add_to_config_reload_watch_list(catppuccin_flavor_file)
 
 local function get_color_scheme()
-   local function get_flavor()
-      local flavor = 'mocha'
-
-      local f = io.open(catppuccin_flavor_file, 'r')
-      if f then
-         flavor = f:read('*all'):gsub('%s+', '')
-         f:close()
-      end
-
-      return flavor
+   local flavor = 'mocha'
+   local f = io.open(catppuccin_flavor_file, 'r')
+   if f then
+      flavor = f:read('*all'):gsub('%s+', '')
+      f:close()
    end
-
-   return 'Catppuccin ' .. (get_flavor():gsub('^%l', string.upper))
+   return 'Catppuccin ' .. flavor:gsub('^%l', string.upper)
 end
 
 return {
    color_scheme = get_color_scheme(),
    disable_default_key_bindings = true,
    font = wezterm.font('JetBrainsMono Nerd Font'),
+   font_size = 13.0,
    front_end = 'WebGpu',
    hide_tab_bar_if_only_one_tab = true,
    keys = {
-      {
-         key = 'v',
-         mods = 'CTRL|SHIFT',
-         action = wezterm.action.PasteFrom('Clipboard'),
-      },
+      { key = 'c', mods = 'SUPER', action = wezterm.action.CopyTo('Clipboard') },
+      { key = 'v', mods = 'SUPER', action = wezterm.action.PasteFrom('Clipboard') },
+      { key = 'c', mods = 'CTRL|SHIFT', action = wezterm.action.CopyTo('Clipboard') },
+      { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.PasteFrom('Clipboard') },
    },
 }
